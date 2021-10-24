6f8d79a3ba7018b647157da9d451871f10a80f0e
SteffenTunkel <48067167+SteffenTunkel@users.noreply.github.com>
2021-10-24 14:32:40 +0200

    Update main.py
    - change to batch-wise processing
    - change from mongoengine lib to pymongo lib
    - add error handling

--- main.py
+++ main.py
@@ -1,44 +1,31 @@
-import os
+import argparse
+import json
 import logging
 import logging.config
-import json
+import os
 import sys
-import argparse
 import timeit
 
-from gdprSHARK.gdprshark import fetch_data, create_and_clean_email_dict, find_and_replace_email
-from pycoshark import mongomodels
-
-from mongoengine import connect
-from pycoshark.mongomodels import Project
-from pycoshark.utils import create_mongodb_uri_string
-from pycoshark.utils import get_base_argparser
+from pycoshark.utils import create_mongodb_uri_string, get_base_argparser
+from pymongo import MongoClient
+from gdprSHARK.gdprshark import load_email_dict, update_db_with_email_filter
 
 
-def setup_logging(default_path=os.path.dirname(os.path.realpath(__file__))+"/loggerConfiguration.json",
+def setup_logging(default_path=os.path.dirname(os.path.realpath(__file__)) + "/loggerConfiguration.json",
                   default_level=logging.INFO):
-        """
-        Setup logging configuration
+    """
+    Setup logging configuration
 
-        :param default_path: path to the logger configuration
-        :param default_level: defines the default logging level if configuration file is not found(default:logging.INFO)
-        """
-        path = default_path
-        if os.path.exists(path):
-            with open(path, 'rt') as f:
-                config = json.load(f)
-            logging.config.dictConfig(config)
-        else:
-            logging.basicConfig(level=default_level)
-
-
-def str_to_mongomodel(str):
-    """Returns mongomodel class named by input string."""
-    try:
-        return getattr(sys.modules["pycoshark.mongomodels"], str)
-    except AttributeError as e:
-        raise RuntimeError(f"Error occured: {e}")
-
+    :param default_path: path to the logger configuration
+    :param default_level: defines the default logging level if configuration file is not found(default:logging.INFO)
+    """
+    path = default_path
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
 
 
 def start():
@@ -49,30 +36,38 @@ def start():
 
     parser = get_base_argparser('', '0.0.1')
     parser.add_argument('--fields', help='Database fields to manipulate',
-                        default="Commit.message,Message.body,Message.subject,Issue.desc,IssueComment.comment,"
-                                "PullRequest.description,PullRequestCommit.message,PullRequestComment.comment")
+                        default="commit.message,message.body,message.subject,issue.desc,issue_comment.comment,"
+                        "pull_request.description,pull_request_commit.message,pull_request_comment.comment")
 
     parser.add_argument('--debug', help='Sets the debug level.', default='DEBUG',
                         choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'])
 
     args = parser.parse_args()
-    uri = create_mongodb_uri_string(args.db_user, args.db_password, args.db_hostname, args.db_port, args.db_authentication, args.ssl)
+    uri = create_mongodb_uri_string(args.db_user, args.db_password, args.db_hostname,
+                                    args.db_port, args.db_authentication, args.ssl)
+    client = MongoClient(uri)
+    db_handle = client[args.db_database]
 
-    connect(args.db_database, host=uri)
-
-    logger.info("load email addresses")
-    email_list = fetch_data(mongomodels.People, ["id", "email"], logger, None, 100000)
-    email_dict = create_and_clean_email_dict(email_list)
+    valid_collections = db_handle.list_collection_names()
+    if "people" not in valid_collections:
+        error_msg = f"'The 'people' collection does not exist in the database. "\
+                    f"Therefore, no email address list can be generated."
+        logger.error(error_msg)
+        raise RuntimeError(error_msg)
 
     for db_field_string in args.fields.split(","):
         db_collection, db_field = db_field_string.split(".")
-        logger.info(f"start loading and replacing of {db_collection}.{db_field}")
-        fields_list = fetch_data(str_to_mongomodel(db_collection), ["id", db_field], logger, None, 100000)
+        if db_collection not in valid_collections:
+            error_msg = f"'{db_collection}' collection does not exist in the database."
+            logger.error(error_msg)
+            raise RuntimeError(error_msg)
 
-        find_and_replace_email(str_to_mongomodel(db_collection), db_field, fields_list, email_dict, logger)
+    email_dict = load_email_dict(db_handle, logger)
 
+    for db_field_string in args.fields.split(","):
+        db_collection, db_field = db_field_string.split(".")
+        update_db_with_email_filter(db_handle, db_collection, db_field, email_dict, logger)
 
 
 if __name__ == "__main__":
     start()
-
